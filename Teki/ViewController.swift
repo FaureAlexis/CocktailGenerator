00294a46ccbde05d3240db12d77085ff49d5e710
Alexis Faure <alexis.faure@epitech.eu>
2022-06-04 18:39:44 +0300

    Initial Commit

---
+++ Teki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Teki
+//
+//  Created by Alexis Faure on 04/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
